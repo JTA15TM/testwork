dda2a9c4b20e9a9f8e40e412240bd084757c9ea5
JTA Team <64577007+JTA15TM@users.noreply.github.com>
2020-11-22 23:51:10 +0300

    Add files via upload

---
+++ feedback/feedback.js
@@ -0,0 +1,11 @@
+get_action = function(form) 
+{
+    var input = document.getElementsByClassName("c-form__input")[0];
+    if(input.value.length >= input.min) 
+    {
+        showSnackbar();
+
+        //Send Feedback
+        /*no function*/
+    }
+}
